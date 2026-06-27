19e74a8747ad3b93b08efb14217f88c27fc1446c
jyoti12724 <jyotigupta402@gmail.com>
2026-06-27 16:45:15 +0530

    Space Removed

--- Artist-JG/App/AppCoordinator.swift
+++ Artist-JG/App/AppCoordinator.swift
@@ -3,17 +3,11 @@ import Foundation
 final class AppCoordinator: ObservableObject {
     @Published var route: AppRoute = .login
 
-    func showLogin() {
-        route = .login
-    }
+    func showLogin() { route = .login }
 
-    func showSignUp() {
-        route = .signUp
-    }
+    func showSignUp() { route = .signUp }
 
-    func showHome(user: User?) {
-        route = .home(user)
-    }
+    func showHome(user: User?) { route = .home(user) }
 
     func logout() {
         AuthTokenStore().clear()
